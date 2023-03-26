c5efba813ba7f3a99757a41172eccd19bbfbbf6e
Siva199221387 <92841952+Siva199221387@users.noreply.github.com>
2023-03-26 18:14:20 +0530

    Add files via upload

---
+++ pratice/src/pratice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class pratice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner s= new Scanner(System.in);
+       System.out.println("enter your text");
+       String text=s.nextLine();
+       System.out.println(text);
+	}
+}
